f2c78c5d2b50d739a05635f5115b8ea2a2aea2d0
sycured <60801403+sycured@users.noreply.github.com>
2022-09-25 03:10:28 -0500

    working encryption

--- src/encrypt.rs
+++ src/encrypt.rs
@@ -1,3 +1,16 @@
+use std::process::Command;
+
 pub fn age(filename: &str, public_key: &str) -> bool {
-    true
+    let filename_with_path = format!("/dump/{}", filename);
+    let status = Command::new("age")
+        .args([
+            "-r",
+            public_key,
+            "-o",
+            format!("{}.age", filename_with_path).as_str(),
+            &filename_with_path,
+        ])
+        .status()
+        .unwrap();
+    status.success()
 }
